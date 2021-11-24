f651d8013ab08a0933da49ac5f621f6e9a48008f
AxelQuintanilla <axel.quintanilla620@gmail.com>
2021-11-23 20:31:58 -0600

    still broken

--- src/main/java/com/telus/ds/TrackApplication.java
+++ src/main/java/com/telus/ds/TrackApplication.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories ("domain.repository-package")
+@ComponentScan(basePackages = {"com.telus.ds.service", "com.telus.ds.entity", "com.telus.ds.controller", "com.telus.ds.entity.dto", "com.telus.ds.repository"})
 @OpenAPIDefinition(info = @Info(title = "Track API", version = "1.0", description = "Track API"))
 public class TrackApplication {
 
