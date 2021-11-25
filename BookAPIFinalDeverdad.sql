b56d43b3ac452bff68066c886abb70c446483e09
AxelQuintanilla <axel.quintanilla620@gmail.com>
2021-11-25 12:23:04 -0600

    Ahora si

--- BookAPIFinalDeverdad.sql
+++ BookAPIFinalDeverdad.sql
@@ -26,14 +26,13 @@ DROP TABLE IF EXISTS `book`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `book` (
   `bookid` int NOT NULL AUTO_INCREMENT,
-  `bookName` varchar(90) NOT NULL,
+  `bookname` varchar(90) NOT NULL,
   `ISBN` varchar(45) NOT NULL,
   `genre` varchar(45) DEFAULT NULL,
   `quantity` int NOT NULL,
   `state` tinyint NOT NULL,
-  PRIMARY KEY (`bookid`),
-  UNIQUE KEY `ISBN_UNIQUE` (`ISBN`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`bookid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +41,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,'Frankenstein','123456789','Terror',5,1),(2,'Java Programming','987654321','Education',10,1),(3,'Harry Potter','123123123','Fantasy',3,1),(4,'Romeo & Juliet','234234234','Romance',5,1),(5,'Differential Calculus I','543543543','Education',3,1),(6,'Manual de Supervivencia Escolar de DSU','6666666666','Terror',6,1);
+INSERT INTO `book` VALUES (1,'Frankenstein','123456789','Terror',5,1),(2,'Learning Java SpringBoot','987654321','Education',10,1),(3,'Harry Potter','123123123','Fantasy',3,1),(4,'Romeo & Juliet','234234234','Romance',5,1),(5,'Differential Calculus II','543543543','Education',3,1),(8,'Libro de prueba','123456543','prueba xd',69,1);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,17 +54,19 @@ DROP TABLE IF EXISTS `borrowedbooks`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `borrowedbooks` (
   `borrowedbooksid` int NOT NULL AUTO_INCREMENT,
-  `return_date` date NOT NULL,
+  `expectedreturn_date` date NOT NULL,
   `borrow_date` date NOT NULL,
   `renewal_quantity` int DEFAULT NULL,
   `bookid` int NOT NULL,
   `userid` int NOT NULL,
+  `returned` tinyint NOT NULL,
+  `returned_date` date DEFAULT NULL,
   PRIMARY KEY (`borrowedbooksid`),
   KEY `bookid_idx` (`bookid`),
   KEY `userid_idx` (`userid`),
   CONSTRAINT `bookid` FOREIGN KEY (`bookid`) REFERENCES `book` (`bookid`),
   CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,36 +75,8 @@ CREATE TABLE `borrowedbooks` (
 
 LOCK TABLES `borrowedbooks` WRITE;
 /*!40000 ALTER TABLE `borrowedbooks` DISABLE KEYS */;
+INSERT INTO `borrowedbooks` VALUES (1,'2021-11-28','2021-11-25',0,1,1,0,NULL),(2,'2021-11-30','2021-11-25',0,2,1,0,NULL),(4,'2021-12-01','2021-11-25',0,1,1,1,'2021-12-02');
 /*!40000 ALTER TABLE `borrowedbooks` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `returnedbooks`
---
-
-DROP TABLE IF EXISTS `returnedbooks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `returnedbooks` (
-  `returnedbooksid` int NOT NULL AUTO_INCREMENT,
-  `returned_date` date NOT NULL,
-  `book2id` int NOT NULL,
-  `user2id` int NOT NULL,
-  PRIMARY KEY (`returnedbooksid`),
-  KEY `book2id_idx` (`book2id`),
-  KEY `user2id_idx` (`user2id`),
-  CONSTRAINT `book2id` FOREIGN KEY (`book2id`) REFERENCES `book` (`bookid`),
-  CONSTRAINT `user2id` FOREIGN KEY (`user2id`) REFERENCES `user` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `returnedbooks`
---
-
-LOCK TABLES `returnedbooks` WRITE;
-/*!40000 ALTER TABLE `returnedbooks` DISABLE KEYS */;
-/*!40000 ALTER TABLE `returnedbooks` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -116,14 +89,11 @@ DROP TABLE IF EXISTS `ticket`;
 CREATE TABLE `ticket` (
   `ticketid` int NOT NULL AUTO_INCREMENT,
   `total` double NOT NULL,
-  `book3id` int NOT NULL,
-  `user3id` int NOT NULL,
+  `borrowedbooksid` int NOT NULL,
   PRIMARY KEY (`ticketid`),
-  KEY `book3id_idx` (`book3id`),
-  KEY `user3id_idx` (`user3id`),
-  CONSTRAINT `book3id` FOREIGN KEY (`book3id`) REFERENCES `book` (`bookid`),
-  CONSTRAINT `user3id` FOREIGN KEY (`user3id`) REFERENCES `user` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `borrowedbooksid_idx` (`borrowedbooksid`),
+  CONSTRAINT `borrowedbooksid` FOREIGN KEY (`borrowedbooksid`) REFERENCES `borrowedbooks` (`borrowedbooksid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,6 +102,7 @@ CREATE TABLE `ticket` (
 
 LOCK TABLES `ticket` WRITE;
 /*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+INSERT INTO `ticket` VALUES (1,0.2,4),(2,0.2,4),(3,0.2,4),(6,2,4),(7,1,4),(8,4,4),(9,10,4);
 /*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +118,7 @@ CREATE TABLE `user` (
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +127,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Axel','Hernandez'),(2,'Julio','Canizales'),(3,'Jackeline','Benitez'),(4,'Cristian','Padilla'),(5,'Susie','Aguilar');
+INSERT INTO `user` VALUES (1,'Axel','Hernandez'),(2,'Julio','Canizales'),(3,'Jackeline','Benitez'),(4,'Cristian','Padilla'),(5,'Susie','Aguilar'),(6,'Tania','Alas');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -169,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-24 12:40:39
+-- Dump completed on 2021-11-25 12:22:27
